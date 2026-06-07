facb77c1a0bc246bf444dda04ce15781e0aaaa9a
Dassaev Tchouta <tchoutadassaev3@gmail.com>
2026-06-07 01:34:36 +0100

    fix API URL for production

--- app/frontend/src/services/api.ts
+++ app/frontend/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+{/* const API_BASE_URL = 'http://localhost:8000'; */}
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://repository-name-fraud-detection-par.onrender.com';
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
